cc53c2e5168d90c07f0c02e46c8320f2927bed7b
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2024-07-25 17:24:07 +0200

    Modified test app to make sure that it has one code and one data segment

--- apps/test/app/src/main.rs
+++ apps/test/app/src/main.rs
@@ -4,6 +4,13 @@
 #[cfg(target_arch = "riscv32")]
 use sdk::fatal;
 
+
+// Temporary to force the creation of a data section
+#[used]
+#[no_mangle]
+pub static mut APP_NAME: [u8; 32] = *b"Test\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+
+
 #[cfg(target_arch = "riscv32")]
 #[panic_handler]
 fn my_panic(_info: &core::panic::PanicInfo) -> ! {
@@ -19,6 +26,22 @@ pub fn _start(_argc: isize, _argv: *const *const u8) -> isize {
 
 #[start]
 pub fn main(_: isize, _: *const *const u8) -> isize {
-    // TODO: implement a main boilerplate
-    loop {}
+    // TODO: remove
+    unsafe {
+        core::ptr::read_volatile(&APP_NAME);
+    }
+
+    sdk::ux::ux_idle();
+    loop {
+        // let buffer = comm::receive_message().unwrap(); // TODO: what to do on error?
+
+        // sdk::ux::app_loading_start("Handling request...\x00");
+
+        // let result = handle_req(&buffer, &mut state);
+
+        // sdk::ux::app_loading_stop();
+        // sdk::ux::ux_idle();
+
+        // comm::send_message(&result).unwrap(); // TODO: what to do on error?
+    }
 }
