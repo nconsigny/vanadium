e4d8a1b511592b1ff55622291da65e32e5770228
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2024-11-22 11:23:09 +0000

    Added minimal speculos integration test suite for vnd-test app

---
+++ apps/test/client/tests/integration_test.rs
@@ -0,0 +1,19 @@
+mod common;
+
+#[tokio::test]
+async fn test_add_numbers() {
+    let mut setup = common::setup().await;
+
+    #[rustfmt::skip]
+    let testcases: Vec<(u32, u64)> = vec![
+        (0, 0),
+        (1, 1),
+        (100, 5050),
+    ];
+
+    for (input, expected) in testcases {
+        assert_eq!(setup.client.add_numbers(input).await.unwrap(), expected);
+    }
+}
+
+// TODO: add other tests
