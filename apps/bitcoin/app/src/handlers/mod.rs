8febcd965428a09103cf895bef23fca0de38fe02
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-01-07 10:19:57 +0100

    Handler for get_master_fingerprint

--- apps/bitcoin/app/src/handlers/mod.rs
+++ apps/bitcoin/app/src/handlers/mod.rs
@@ -1,8 +1,19 @@
 use common::message::ResponseGetMasterFingerprint;
+use sdk::curve::Curve;
 
 pub fn handle_get_master_fingerprint() -> Result<ResponseGetMasterFingerprint, &'static str> {
-    // TODO: replace with proper sdk call
     Ok(ResponseGetMasterFingerprint {
-        fingerprint: 0xf5acc2fd,
+        fingerprint: sdk::curve::Secp256k1::get_master_fingerprint(),
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_handle_get_master_fingerprint() {
+        let response = handle_get_master_fingerprint().unwrap();
+        assert_eq!(response.fingerprint, 0xf5acc2fdu32);
+    }
 }
