df4589dd05d021c37f030f01626a6045248ea32c
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-11 10:59:00 +0100

    Delete ellswift and ecdh modules

--- libs/secp256k1/src/key.rs
+++ libs/secp256k1/src/key.rs
@@ -9,7 +9,6 @@ use core::{fmt, ptr, str};
 #[cfg(feature = "serde")]
 use serde::ser::SerializeTuple;
 
-use crate::ellswift::ElligatorSwift;
 use crate::ffi::types::c_uint;
 use crate::ffi::{self, CPtr};
 use crate::Error::{self, InvalidPublicKey, InvalidPublicKeySum, InvalidSecretKey};
@@ -428,9 +427,6 @@ impl PublicKey {
             PublicKey(pk)
         }
     }
-    /// Creates a new public key from an [`ElligatorSwift`].
-    #[inline]
-    pub fn from_ellswift(ellswift: ElligatorSwift) -> PublicKey { ElligatorSwift::decode(ellswift) }
 
     /// Creates a new public key from a [`SecretKey`] and the global [`SECP256K1`] context.
     #[inline]
