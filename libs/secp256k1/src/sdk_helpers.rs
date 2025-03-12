2e9cc0706af28d30507fcabd46b60ab5e8254cb1
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-12 15:13:20 +0100

    Fix deserialization of compressed public keys

--- libs/secp256k1/src/sdk_helpers.rs
+++ libs/secp256k1/src/sdk_helpers.rs
@@ -26,14 +26,21 @@ pub fn secp256k1_compute_y<'a>(x: &'a BigNumMod<'a, 32>) -> Result<BigNumMod<'a,
     secp256k1_compute_y_internal(x).ok_or(InvalidPublicKey)
 }
 
-// Computes the even y-coordinate using (x^3 + 7)^SQR_EXPONENT
+// Computes the y-coordinate using (x^3 + 7)^SQR_EXPONENT with the given parity
 // It returns InvalidPublicKey if the x does not have a corresponding y on the curve
 #[inline]
-pub fn secp256k1_compute_even_y<'a>(x: &'a BigNumMod<'a, 32>) -> Result<BigNumMod<'a, 32>, Error> {
+pub fn secp256k1_compute_y_with_parity<'a>(
+    x: &'a BigNumMod<'a, 32>,
+    parity: u8,
+) -> Result<BigNumMod<'a, 32>, Error> {
+    if parity > 1 {
+        panic!("This function must be called with parity equal to exactly 0 or 1");
+    }
+
     let mut y = secp256k1_compute_y_internal(x).ok_or(InvalidPublicKey)?;
 
     // if the last byte of y is odd, negate it to ensure an even y-coordinate
-    if y.as_be_bytes()[31] & 1 != 0 {
+    if y.as_be_bytes()[31] & 1 != parity {
         let zero = BigNumMod::from_u32(0, &P);
         y = &zero - &y;
     }
