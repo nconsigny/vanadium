532194bbc49beac27cbcb06a10dec8724a99fab5
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-18 09:12:04 +0100

    Removed unused imports and macros

--- libs/bitcoin-hashes/src/util.rs
+++ libs/bitcoin-hashes/src/util.rs
@@ -66,35 +66,6 @@ macro_rules! borrow_slice_impl(
     )
 );
 
-macro_rules! engine_input_impl(
-    () => (
-        #[cfg(not(hashes_fuzz))]
-        fn input(&mut self, mut inp: &[u8]) {
-            while !inp.is_empty() {
-                let buf_idx = self.length % <Self as crate::HashEngine>::BLOCK_SIZE;
-                let rem_len = <Self as crate::HashEngine>::BLOCK_SIZE - buf_idx;
-                let write_len = cmp::min(rem_len, inp.len());
-
-                self.buffer[buf_idx..buf_idx + write_len]
-                    .copy_from_slice(&inp[..write_len]);
-                self.length += write_len;
-                if self.length % <Self as crate::HashEngine>::BLOCK_SIZE == 0 {
-                    self.process_block();
-                }
-                inp = &inp[write_len..];
-            }
-        }
-
-        #[cfg(hashes_fuzz)]
-        fn input(&mut self, inp: &[u8]) {
-            for c in inp {
-                self.buffer[0] ^= *c;
-            }
-            self.length += inp.len();
-        }
-    )
-);
-
 /// Creates a new newtype around a [`Hash`] type.
 ///
 /// The syntax is similar to the usual tuple struct syntax:
