6f85dd4c3d537bd9b61469709bed906a0e0cedd2
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-28 09:45:09 +0100

    Refactor bitcoin app to use the new App from the sdk

--- apps/bitcoin/app/src/handlers/get_master_fingerprint.rs
+++ apps/bitcoin/app/src/handlers/get_master_fingerprint.rs
@@ -1,7 +1,7 @@
 use common::message::Response;
 use sdk::curve::Curve;
 
-pub fn handle_get_master_fingerprint() -> Result<Response, &'static str> {
+pub fn handle_get_master_fingerprint(_app: &mut sdk::App) -> Result<Response, &'static str> {
     Ok(Response::MasterFingerprint(
         sdk::curve::Secp256k1::get_master_fingerprint(),
     ))
@@ -13,7 +13,7 @@ mod tests {
 
     #[test]
     fn test_handle_get_master_fingerprint() {
-        let response = handle_get_master_fingerprint().unwrap();
+        let response = handle_get_master_fingerprint(&mut sdk::App::singleton()).unwrap();
         assert_eq!(response, Response::MasterFingerprint(0xf5acc2fdu32));
     }
 }
