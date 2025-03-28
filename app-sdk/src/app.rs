bae296e47c7da7ff520d740eeff24eae3eff6c60
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-03-28 16:26:50 +0100

    Improve docs of the sdk::app module

--- app-sdk/src/app.rs
+++ app-sdk/src/app.rs
@@ -1,17 +1,24 @@
 use alloc::vec::Vec;
 
+/// A Handler is a function that is called when a message is received from the host, and
+/// returns the app's response.
 pub type Handler = fn(&mut App, &[u8]) -> Vec<u8>;
 
+/// The App struct represents the context of the application.
 pub struct App {
     handler: Handler,
 }
 
 impl App {
+    /// Creates a new App instance with the given handler.
     pub fn new(handler: Handler) -> Self {
         App { handler }
     }
 
-    pub fn run(&mut self) {
+    /// This function shows the dashboard, then enters the core loop of the app.
+    /// It never returns, as it keeps the app running until sdk::exit() is called,
+    /// or a fatal error occurs.
+    pub fn run(&mut self) -> ! {
         crate::ux::ux_idle();
         loop {
             // TODO: can we handle the error any better?
@@ -22,6 +29,7 @@ impl App {
         }
     }
 
+    /// This is only useful to produce a valid app instance in tests.
     pub fn singleton() -> Self {
         App::new(|_app, _msg| Vec::new())
     }
