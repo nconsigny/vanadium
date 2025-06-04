e8e157c6b6615d1ddf98b199d41be343c69a0326
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-06-04 11:17:55 +0200

    Use a random key stored in NVM as the V-App registration key

--- vm/src/handlers/lib/vapp.rs
+++ vm/src/handlers/lib/vapp.rs
@@ -3,6 +3,64 @@ use ledger_device_sdk::hmac::{self, HMACInit};
 
 use crate::hash::Sha256Hasher;
 
+use ledger_device_sdk::nvm::*;
+use ledger_device_sdk::NVMData;
+
+/// Encapsulates the key used for the V-App registration.
+/// It is generated on first use, and stored in the NVM.
+pub struct VappRegistrationKey;
+
+impl Default for VappRegistrationKey {
+    fn default() -> Self {
+        VappRegistrationKey
+    }
+}
+
+// We use the initial value (all zeros) to mark the key as uninitialized.
+// We generate a new random key at first use.
+
+#[link_section = ".nvm_data"]
+static mut VAPP_REGISTRATION_KEY: NVMData<AtomicStorage<[u8; 32]>> =
+    NVMData::new(AtomicStorage::new(&[0u8; 32]));
+
+// check whether the key is all zeros with a constant time comparison
+fn is_all_zeros_ct(data: &[u8]) -> bool {
+    let mut data_or = 0u8;
+    for &byte in data.iter() {
+        data_or |= byte;
+    }
+    data_or == 0
+}
+
+impl VappRegistrationKey {
+    fn ensure_initialized() {
+        // if the key is all zeros, initialize it with 32 random bytes
+        let nvm_key = &raw mut VAPP_REGISTRATION_KEY;
+        unsafe {
+            let storage = (*nvm_key).get_mut();
+
+            // check whether the key is all zeros with a constant time comparison
+            if is_all_zeros_ct(storage.get_ref().as_slice()) {
+                let mut new_key = [0u8; 32];
+                ledger_device_sdk::random::rand_bytes(&mut new_key);
+                storage.update(&new_key);
+            }
+        }
+    }
+
+    #[inline(never)]
+    pub fn get_ref(&mut self) -> &AtomicStorage<[u8; 32]> {
+        Self::ensure_initialized();
+
+        let data = &raw const VAPP_REGISTRATION_KEY;
+        unsafe { (*data).get_ref() }
+    }
+
+    pub fn get_key(&mut self) -> &[u8; 32] {
+        self.get_ref().get_ref()
+    }
+}
+
 /// Computes the HMAC for the V-App.
 ///
 /// SECURITY: The caller is responsible for ensuring that comparisons involving the
@@ -10,10 +68,9 @@ use crate::hash::Sha256Hasher;
 pub fn get_vapp_hmac(manifest: &Manifest) -> [u8; 32] {
     let vapp_hash: [u8; 32] = manifest.get_vapp_hash::<Sha256Hasher, 32>();
 
-    // TODO: derive a key using SLIP-21, or otherwise initialize it elsewhere.
-    let hmac_key = [42u8; 32];
+    let mut vapp_key = VappRegistrationKey;
 
-    let mut sha2 = hmac::sha2::Sha2_256::new(&hmac_key);
+    let mut sha2 = hmac::sha2::Sha2_256::new(vapp_key.get_key());
     sha2.update(&vapp_hash).expect("Should never fail");
     let mut vapp_hmac = [0u8; 32];
     sha2.finalize(&mut vapp_hmac).expect("Should never fail");
