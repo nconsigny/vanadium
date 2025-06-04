1c193d7d07471019b520cd71d00df34f2b21ec97
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-06-04 16:55:46 +0200

    Delete boilerplate settings

--- vm/src/app_ui/menu.rs
+++ vm/src/app_ui/menu.rs
@@ -1,7 +1,6 @@
 use include_gif::include_gif;
 use ledger_device_sdk::{io::Comm, nbgl::NbglHomeAndSettings};
 
-use crate::settings::Settings;
 use ledger_device_sdk::nbgl::NbglGlyph;
 
 pub fn ui_menu_main(_: &mut Comm) -> NbglHomeAndSettings {
@@ -13,16 +12,10 @@ pub fn ui_menu_main(_: &mut Comm) -> NbglHomeAndSettings {
     const VANADIUM_ICON: NbglGlyph =
         NbglGlyph::from_include(include_gif!("vanadium_16x16.gif", NBGL));
 
-    let settings_strings = [["Test", "Test setting."]];
-    let mut settings: Settings = Default::default();
-
     // Display the home screen.
-    NbglHomeAndSettings::new()
-        .glyph(&VANADIUM_ICON)
-        .settings(settings.get_mut(), &settings_strings)
-        .infos(
-            "Vanadium",
-            env!("CARGO_PKG_VERSION"),
-            env!("CARGO_PKG_AUTHORS"),
-        )
+    NbglHomeAndSettings::new().glyph(&VANADIUM_ICON).infos(
+        "Vanadium",
+        env!("CARGO_PKG_VERSION"),
+        env!("CARGO_PKG_AUTHORS"),
+    )
 }
