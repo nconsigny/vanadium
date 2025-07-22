555b79c90e52ff6ff3b3fff50426db68d600beb3
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-07-22 11:49:44 +0200

    Remove unnecessary feature from the common crate
    It complicates the dependency graph, and it's easy to replace.

--- vm/src/handlers/lib/mod.rs
+++ vm/src/handlers/lib/mod.rs
@@ -1,3 +1,15 @@
+use common::client_commands::Message;
+
 pub mod ecall;
 pub mod outsourced_mem;
 pub mod vapp;
+
+trait SerializeToComm {
+    fn serialize_to_comm(&self, comm: &mut ledger_device_sdk::io::Comm);
+}
+
+impl<'a, T: Message<'a>> SerializeToComm for T {
+    fn serialize_to_comm(&self, comm: &mut ledger_device_sdk::io::Comm) {
+        self.serialize_with(|data| comm.append(data));
+    }
+}
