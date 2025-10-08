01811a5bdbe649940a35637071fd4bd9c50c22f1
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-10-08 17:21:59 +0200

    Avoid floating point arithmetic for the fee warning
    Using f64 increases the binary size by over 18kb.

--- apps/bitcoin/app/src/constants.rs
+++ apps/bitcoin/app/src/constants.rs
@@ -1,7 +1,7 @@
 pub const COIN_TICKER: &'static str = "TEST";
 
 // For amount (in sats) not smaller than THRESHOLD_WARN_HIGH_FEES_AMOUNT, we show a warning
-// if the amount spend in fees divided by the total amount of all inputs) is greater than
-// or equal THRESHOLD_WARN_HIGH_FEES_FRACTION.
-pub const THRESHOLD_WARN_HIGH_FEES_FRACTION: f64 = 0.1;
+// if the percentage (in whole percents) of fees over total input amount is greater than or
+// equal to THRESHOLD_WARN_HIGH_FEES_PERCENT. (E.g. 10 means 10%).
+pub const THRESHOLD_WARN_HIGH_FEES_PERCENT: u64 = 10; // 10%
 pub const THRESHOLD_WARN_HIGH_FEES_AMOUNT: u64 = 100_000;
