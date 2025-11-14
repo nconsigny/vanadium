ad3b44ca67a47b03ed947070f5d85c0787303124
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-11-14 17:04:41 +0100

    Fix icon size for apex_p

--- vm/src/handlers/lib/ecall/bitmaps.rs
+++ vm/src/handlers/lib/ecall/bitmaps.rs
@@ -3,7 +3,7 @@ pub trait ToIconDetails {
     fn to_icon_details(&self) -> *const nbgl_icon_details_t;
 }
 
-#[cfg(any(target_os = "stax", target_os = "flex", target_os = "apex_p"))]
+#[cfg(any(target_os = "stax", target_os = "flex"))]
 mod large_screen {
     use super::*;
     use ledger_secure_sdk_sys::{nbgl_icon_details_t, NBGL_BPP_4};
@@ -29,6 +29,40 @@ mod large_screen {
                 common::ux::Icon::None => core::ptr::null(),
                 common::ux::Icon::Success => &CHECK_CIRCLE_64PX,
                 common::ux::Icon::Failure => &DENIED_CIRCLE_64PX,
+                common::ux::Icon::Confirm => core::ptr::null(), // only for small screen devices
+                common::ux::Icon::Reject => core::ptr::null(),  // only for small screen devices
+                common::ux::Icon::Processing => core::ptr::null(), // only for small screen devices
+            }
+        }
+    }
+}
+
+#[cfg(target_os = "apex_p")]
+mod apex_screen {
+    use super::*;
+    use ledger_secure_sdk_sys::{nbgl_icon_details_t, NBGL_BPP_4};
+
+    const CHECK_CIRCLE_48PX: nbgl_icon_details_t = nbgl_icon_details_t {
+        width: 48,
+        height: 48,
+        bpp: NBGL_BPP_4,
+        isFile: true,
+        bitmap: unsafe { &ledger_secure_sdk_sys::C_Check_Circle_48px_bitmap } as *const u8,
+    };
+    const DENIED_CIRCLE_48PX: nbgl_icon_details_t = nbgl_icon_details_t {
+        width: 48,
+        height: 48,
+        bpp: NBGL_BPP_4,
+        isFile: true,
+        bitmap: unsafe { &ledger_secure_sdk_sys::C_Denied_Circle_48px_bitmap } as *const u8,
+    };
+
+    impl ToIconDetails for common::ux::Icon {
+        fn to_icon_details(&self) -> *const nbgl_icon_details_t {
+            match self {
+                common::ux::Icon::None => core::ptr::null(),
+                common::ux::Icon::Success => &CHECK_CIRCLE_48PX,
+                common::ux::Icon::Failure => &DENIED_CIRCLE_48PX,
                 common::ux::Icon::Confirm => core::ptr::null(), // only for small screen devices
                 common::ux::Icon::Reject => core::ptr::null(),  // only for small screen devices
                 common::ux::Icon::Processing => core::ptr::null(), // only for small screen devices
