8fb9dc66999d6535b5603752f13f8ac847a8d7d8
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-11-25 11:28:48 +0100

    Add generated template

---
+++ apps/template/generate/README.md
@@ -0,0 +1,65 @@
+This is a Boilerplate V-App.
+
+- [app](app) contains the Risc-V app for Vanadium.
+- [client](client) folder contains the client of the app, and a simple CLI interface.
+
+The `client` is a library crate (see [lib.rs](client/src/lib.rs)), but it also has a test executable ([main.rs](client/src/main.rs)) to interact with the app from the command line.
+
+## Build the V-App
+
+If you installed [just](https://github.com/casey/just), simply running `just` from the `app` folder will build both the
+Risc-V and the native binaries.
+
+### Risc-V
+
+In order to build the app for the Risc-V target, enter the `app` folder and run:
+
+   ```sh
+   cargo build --release --target=riscv32imc-unknown-none-elf
+   ```
+
+### Native
+
+In order to build the app for the native target, enter the `app` folder and run:
+
+   ```sh
+  cargo build --release --target=x86_64-unknown-linux-gnu
+   ```
+
+## Run the V-App
+
+### Native target
+
+Make sure you built the V-App for the native target.
+
+On a terminal in the `app` folder, simply run:
+
+   ```sh
+   cargo run
+   ```
+
+On a different terminal in the `client` folder, run:
+
+   ```sh
+   cargo run -- --native
+   ```
+
+Note: you can customize the hostname and port of the app by setting the `VAPP_ADDRESS` environment variable.
+
+### RISC-V
+
+Make sure you built the V-App for the RISC-V target.
+
+Launch Vanadium on speculos. Then execute:
+
+From the `client` folder
+
+   ```sh
+   cargo run
+   ```
+
+If you want to run the V-app on a real device, launch the Vanadium app on the device, then execute instead:
+
+   ```sh
+   cargo run -- --hid
+   ```
---
+++ apps/template/generate/client/src/main.rs
@@ -0,0 +1,65 @@
+use clap::Parser;
+use vnd_template_client::Client;
+
+use sdk::vanadium_client::client_utils::{create_default_client, ClientType};
+use std::io::BufRead;
+
+#[derive(Parser)]
+#[command(name = "Template", about = "Run the Template V-App on Vanadium")]
+struct Args {
+    /// Path to the ELF file of the V-App (if not the default one)
+    app: Option<String>,
+
+    /// Use the HID interface for a real device, instead of Speculos
+    #[arg(long, group = "interface")]
+    hid: bool,
+
+    /// Use the native interface
+    #[arg(long, group = "interface")]
+    native: bool,
+}
+
+#[tokio::main(flavor = "multi_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    if args.hid && args.native {
+        eprintln!("The --native and --hid options are mutually exclusive.");
+        std::process::exit(1);
+    }
+
+    let client_type = if args.hid {
+        ClientType::Hid
+    } else if args.native {
+        ClientType::Native
+    } else {
+        ClientType::Tcp
+    };
+    let mut client = Client::new(create_default_client("{{project-app-crate}}", client_type, None).await?);
+
+    loop {
+        println!("Enter the message to be signed (or empty to exit):");
+        let mut msg = String::new();
+        std::io::stdin()
+            .lock()
+            .read_line(&mut msg)
+            .expect("Failed to read line");
+
+        let msg = msg.trim().as_bytes();
+        if msg.is_empty() {
+            break;
+        }
+
+        let sig = client.sign_message(msg).await?;
+
+        if sig.is_empty() {
+            println!("Signature rejected");
+        } else {
+            let sig_hex = sig.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+            println!("Received signature: {}", sig_hex);
+        }
+    }
+    let exit_code = client.exit().await?;
+    println!("V-App exited with code: {}", exit_code);
+    Ok(())
+}
